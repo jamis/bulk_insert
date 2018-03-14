b355108a49ece9bec379cf0a48bd42be7f3ebcac
Peter Loomis <peter.loomis@procore.com>
2018-03-14 13:16:42 -0700

    remove redundant self

--- lib/bulk_insert.rb
+++ lib/bulk_insert.rb
@@ -6,7 +6,7 @@ module BulkInsert
   module ClassMethods
     def bulk_insert(*columns, values: nil, set_size:500, ignore: false, update_duplicates: false, return_primary_keys: false)
       columns = default_bulk_columns if columns.empty?
-      worker = BulkInsert::Worker.new(connection, table_name, self.primary_key, columns, set_size, ignore, update_duplicates, return_primary_keys)
+      worker = BulkInsert::Worker.new(connection, table_name, primary_key, columns, set_size, ignore, update_duplicates, return_primary_keys)
 
       if values.present?
         transaction do
