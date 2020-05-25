05cb280840783f2388f3e6f0340c048d97392536
Mauro Berlanda <mauro.berlanda@gmail.com>
2020-05-26 00:36:57 +0200

    ci: fix few more tests

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -18,7 +18,8 @@ module Dummy
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
-    config.active_record.sqlite3.represent_boolean_as_integer = true
+    if Gem.loaded_specs['rails'].version >= Gem::Version.new('5.0.0')
+      config.active_record.sqlite3.represent_boolean_as_integer = true
+    end
   end
 end
