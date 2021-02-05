799c222a1503d2f1519474b880ed77c7ab983a0d
Mauro Berlanda <mauro.berlanda@gmail.com>
2021-02-06 00:33:15 +0100

    removed rails 3 testing, fixed rails 4 and 6

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -18,7 +18,38 @@ module Dummy
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    if Gem.loaded_specs['rails'].version >= Gem::Version.new('5.0.0')
+    RAILS_VERSION = Gem.loaded_specs['rails'].version
+
+    # Patch SQLite to support multiple rails versions with the same app
+    # Tests are written assuming booleans as integers
+    # https://github.com/rails/rails/commit/a18cf23a9cbcbeed61e8049442640c7153e0a8fb
+    if RAILS_VERSION < Gem::Version.new('5.2.0')
+      # https://github.com/rails/rails/commit/52e050ed00b023968fecda82f19a858876a7c435
+      require 'active_record/connection_adapters/sqlite3_adapter'
+      ActiveRecord::ConnectionAdapters::SQLite3Adapter.class_eval do
+        class_attribute :represent_boolean_as_integer, default: false
+      # end
+      # ActiveRecord::ConnectionAdapters::SQLite3::Quoting.module_eval do
+        def quoted_true
+          ActiveRecord::ConnectionAdapters::SQLite3Adapter.represent_boolean_as_integer ? "1".freeze : "'t'".freeze
+        end
+
+        def unquoted_true
+          ActiveRecord::ConnectionAdapters::SQLite3Adapter.represent_boolean_as_integer ? 1 : "t".freeze
+        end
+
+        def quoted_false
+          ActiveRecord::ConnectionAdapters::SQLite3Adapter.represent_boolean_as_integer ? "0".freeze : "'f'".freeze
+        end
+
+        def unquoted_false
+          ActiveRecord::ConnectionAdapters::SQLite3Adapter.represent_boolean_as_integer ? 0 : "f".freeze
+        end
+      end
+
+      ActiveRecord::ConnectionAdapters::SQLite3Adapter.represent_boolean_as_integer = true
+    # https://github.com/rails/rails/commit/f59b08119bc0c01a00561d38279b124abc82561b
+    elsif RAILS_VERSION < Gem::Version.new('6.1.0')
       config.active_record.sqlite3.represent_boolean_as_integer = true
     end
   end
